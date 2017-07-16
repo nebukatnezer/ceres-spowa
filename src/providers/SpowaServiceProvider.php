4642f97e180a166408cd3f337c7b450cc8fec515
nebukatnezer <ralf.liebaug@gmail.com>
2017-07-16 22:31:03 +0200

    Create SpowaServiceProvider.php

---
+++ src/providers/SpowaServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace Spowa\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class SpowaServiceProvider extends ServiceProvider
+{
+ 
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+ 
+	}
+}
