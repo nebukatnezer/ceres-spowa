472daf21658fd8cc856bc9550e6769e1b1662bc3
nebukatnezer <ralf.liebaug@gmail.com>
2017-07-18 13:39:11 +0200

    Update SpowaContainer.php

--- src/Containers/SpowaContainer.php
+++ src/Containers/SpowaContainer.php
@@ -1,1 +1,13 @@
-Test
+<?php
+ 
+namespace Spowa\Containers;
+ 
+use Plenty\Plugin\Templates\Twig;
+ 
+class SpowaContainer
+{
+    public function call(Twig $twig):string
+    {
+        return $twig->render('Spowa::content.Spowa');
+    }
+}
