4b0bccb709bd9f9a02a4a8cbc382104e61d5b3a8
nebukatnezer <ralf.liebaug@gmail.com>
2017-07-18 13:37:25 +0200

    Update SpowaServiceProvider.php

--- src/Providers/SpowaServiceProvider.php
+++ src/Providers/SpowaServiceProvider.php
@@ -1,1 +1,32 @@
-Test
+<?php
+ 
+namespace Spowa\Providers;
+ 
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+ 
+class SpowaServiceProvider extends ServiceProvider
+{
+ 
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+ 
+	}
+ 
+    /**
+	 * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+	 */
+	public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+        {
+           $partial->set('footer', 'Spowa::content.SpowaFooter');
+        }, 0);
+        return false;
+    }
+}
